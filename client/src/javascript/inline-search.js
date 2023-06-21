9083225d259262fdad7e9d15562c209b79466b1a
Bettina Bröthaler <bettina.broethaler@gmx.at>
2023-06-21 09:48:55 +0200

    Changed all search ids to classes

--- client/src/javascript/inline-search.js
+++ client/src/javascript/inline-search.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let openBtn = document.querySelector('#open-inline-search-btn');
-    if(!openBtn) return;
+    let openBtns = document.querySelectorAll('.open-inline-search-btn');
 
-    openBtn.addEventListener('click', (e) => {
-        let searchBars = document.querySelectorAll('.inline-search-holder');
+    openBtns.forEach(openBtn => {
+        openBtn.addEventListener('click', (e) => {
+            let searchBars = document.querySelectorAll('.inline-search-holder');
 
-        searchBars.forEach((searchBar) => {
-            let search = searchBar.querySelector('.inline-search');
-            let searchInput = search.querySelector('.search-pattern');
-            if(!searchInput) return;
+            searchBars.forEach((searchBar) => {
+                let search = searchBar.querySelector('.inline-search');
+                let searchInput = search.querySelector('.search-pattern');
+                if(!searchInput) return;
 
-            search.classList.remove('hidden');
-            openBtn.style.display = 'none';
-            searchInput.focus();
+                search.classList.remove('hidden');
+                openBtn.style.display = 'none';
+                searchInput.focus();
 
-            let closeBtn = search.querySelector('.close-inline-search-btn');
-            if(closeBtn) {
-                closeBtn.addEventListener('click', () => {
-                    search.classList.add('hidden');
-                    openBtn.style.display = 'inline-block';
-                    searchInput.value = '';
-                })
-            }
-        })
+                let closeBtn = search.querySelector('.close-inline-search-btn');
+                if(closeBtn) {
+                    closeBtn.addEventListener('click', () => {
+                        search.classList.add('hidden');
+                        openBtn.style.display = 'inline-block';
+                        searchInput.value = '';
+                    })
+                }
+            })
+        });
     });
+
 });
