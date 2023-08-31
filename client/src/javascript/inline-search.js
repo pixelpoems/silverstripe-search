3751cf002da966223fa452f4944938ca08dbdce2
Bettina Bröthaler <bettina.broethaler@gmx.at>
2023-08-31 12:05:55 +0200

    Updated close button to be unconnected with open button

--- client/src/javascript/inline-search.js
+++ client/src/javascript/inline-search.js
@@ -1,31 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
     let openBtns = document.querySelectorAll('.btn__search');
-
-    openBtns.forEach(openBtn => {
-        openBtn.addEventListener('click', (e) => {
-            let searchBars = document.querySelectorAll('.search-holder__inline');
-
-            searchBars.forEach((searchBar) => {
-                let search = searchBar.querySelector('.search-bar');
-                let searchInput = search.querySelector('.search-input');
-                if(!searchInput) return;
-
-                search.classList.add('active');
-                searchInput.focus();
-                let closeBtn = search.querySelector('.btn__close');
-                if(closeBtn) {
-                    closeBtn.addEventListener('click', () => {
-                        search.classList.remove('active');
-                        openBtn.style.display = 'inline-block';
-                        searchInput.value = '';
-
-                        let resultElement = searchBar.querySelector('.js-result-list');
-                        resultElement.innerHTML = '';
-                        resultElement.classList.add('hidden');
-                    })
-                }
-            })
+    if(openBtns) {
+        openBtns.forEach(openBtn => {
+            openBtn.addEventListener('click', (e) => {
+                let searchBars = document.querySelectorAll('.search-holder__inline');
+                searchBars.forEach((searchBar) => {
+                    let search = searchBar.querySelector('.search-bar');
+                    let searchInput = search.querySelector('.search-input');
+                    if(!searchInput) return;
+                    search.classList.add('active');
+                    searchInput.focus();
+                })
+            });
         });
-    });
+    }
 
+    let closeBtns = document.querySelectorAll('.btn__close');
+    if(closeBtns) {
+        closeBtns.forEach(closeBtn => {
+            closeBtn.addEventListener('click', () => {
+                search.classList.remove('active');
+                openBtn.style.display = 'inline-block';
+                searchInput.value = '';
+                let resultElement = searchBar.querySelector('.js-result-list');
+                resultElement.innerHTML = '';
+                resultElement.classList.add('hidden');
+            })
+        })
+    }
 });
